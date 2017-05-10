a8f27be541baf1d00c09fc1b797fac64fbcb5926
Glen Turner <gturner0316@yahoo.com>
2017-05-10 00:13:58 -0400

    update/delete

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
     // GET route for getting all of the burgers
     app.get("/burgers", function (req, res) {
         // findAll returns all entries for a table when used with no options
-        db.Burger.findAll({}).then(function (data) {
+        db.burger.findAll({}).then(function (data) {
             var hbsObject = {burgers: data};
 
             res.render('index', hbsObject);
@@ -25,63 +25,53 @@ module.exports = function(app) {
         // insert into our table. In this case we just we pass in an object with a text
         // and complete property (req.body)
         console.log("This is req.body " + JSON.stringify(req.body));
-        db.Burger.create({
+        db.burger.create({
             burger_name: req.body.burger_name
 
         }).then(function(data) {
             res.redirect('/burgers');
         });
     });
+
+    // PUT route for updating burgers. We can get the updated todo data from req.body
+    app.put("/burgers/update/:id", function(req, res) {
+        // Update takes in an object describing the properties we want to update, and
+        // we use where to describe which objects we want to update
+        db.burger.update({
+           /* burger_name: req.body.burger_name,*/
+            devoured: 1
+        }, {
+            where: {
+                id: req.params.id
+            }
+        }).then(function(data) {
+            res.json(data);
+        });
+        res.redirect("/burgers");
+        // Testing/Debugging //
+        console.log("SMASHED!");
+    });
+
+// DELETE route for deleting burgers. We can get the id of the todo to be deleted from
+// req.params.id
+    app.delete("/burgers/delete/:id", function(req, res) {
+        // We just have to specify which todo we want to destroy with "where"
+        db.burger.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(data) {
+            res.json(data);
+        });
+        res.redirect("/burgers");
+        // Testing/Debugging //
+        console.log("TRASHED!");
+
+    });
+
+
 };
 
 
-
-
-
-/*
-router.get('/', function(req, res){
-    res.redirect('/burgers')
-});
-// Land on endpoint /burgers function will initialize ORM ALL function which will enter callback hell
-// // Create routes render page with all objects from burgers table in burgers_db
-router.get('/burgers', function(req, res){
-    burgers.all(function(data){
-        var hbsObject = {burgers: data};
-        // Testing/Debugging //
-        console.log(colors.inverse.red("CONTROLLER data " + JSON.stringify(data)));
-
-        console.log(colors.inverse.red("CONTROLLER Hbs Object" + JSON.stringify(hbsObject)));
-
-        res.render('index', hbsObject);
-    });
-});
-
-router.post('/burgers/create', function(req, res){
-    burgers.create(['burger_name'], [req.body.b_name], function(data){
-        res.redirect('/burgers');
-        // Testing/Debugging //
-        console.log(colors.inverse.red("CONTROLLER req.body.b_name: " + JSON.stringify(req.body.b_name)));
-    });
-});
-
-router.put('/burgers/update/:id', function(req, res){
-    var condition = 'id = ' + req.params.id;
-
-    console.log(colors.inverse.red('CONTROLLER condition ', condition));
-
-    burgers.update({'devoured': req.body.devoured}, condition, function(data){
-        res.redirect('/burgers');
-    });
-});
-
-router.delete('/burgers/delete/:id', function(req,res){
-    var condition = 'id = ' + req.params.id;
-    burgers.delete(condition, function (){
-        res.redirect("/burgers");
-    });
-});
-
-module.exports = router;
-*/
 
 
--- models/burger.js
+++ models/burger.js
@@ -1,13 +1,17 @@
 // Import the ORM to create functions that will interact with the database.
 
-module.exports = function(sequelize, DataTypes) {
-    var Burger = sequelize.define("Burger", {
-        burger_name: DataTypes.STRING,
-        Smashed: {
+module.exports = function (sequelize, DataTypes) {
+    var burger = sequelize.define("burger", {
+        burger_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {len: [1]}
+        },
+        devoured: {
             type: DataTypes.BOOLEAN,
             allowNull: false,
             defaultValue: false,
         },
     });
-    return Burger;
+    return burger;
 };
